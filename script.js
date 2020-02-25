555e2a919fcea588a8f4c37dff7da4dfdef60064
pavliuk <Dima>
2020-02-25 12:23:01 +0200

    fix script

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 setInterval(()=> {
-    let button = document.querySelectorAll('ytd-popup-container #cancel-button');
+    let button = document.querySelector('ytd-popup-container #cancel-button');
     if(!button) return;
     let click = new Event('click');
     console.log('#cancel-button', button);
