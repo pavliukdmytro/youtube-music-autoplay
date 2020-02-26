c6c949604ea256a3506d37b7612718e191e7cdec
pavliuk <Dima>
2020-02-26 11:31:19 +0200

    поправил проверку когда кнопка display none

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 setInterval(()=> {
     let button = document.querySelector('ytd-popup-container #cancel-button');
-    if(!button && getComputedStyle(button).display !== 'none') return;
+    if(!button && getComputedStyle(button).display === 'none') return;
     let click = new Event('click');
     console.log('click on #cancel-button', button);
     button.dispatchEvent(click);
